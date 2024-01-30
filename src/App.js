5335d3a957cbe81f27f45e0cbcabec2815de15bb
Diogo <diogomeira81@gmail.com>
2024-01-30 21:43:35 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import {useState} from 'react';
+
+function App() {
+  return (
+    <div className="flex justify-center align-center">
+      <div className='flex bg-white w-4/5'>
+        <h1>To do List</h1>
+        <p>Image</p>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
